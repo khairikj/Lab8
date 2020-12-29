70ab5a42d78a2d15728ba1ef00a41ec9839549e1
khairikj99 <khairi.ygk99@gmail.com>
2020-12-29 22:23:11 +0800

    commit 1

---
+++ android/app/src/main/kotlin/com/example/aws_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aws_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:dd331f97-3cd5-4456-9ed8-c7ecf0525649",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_JMZlayoZt",
+                        "AppClientId": "1306ibeoouo65slon3cq0i5lgc",
+                        "AppClientSecret": "199m99h9ic0jp2c4t50se4ii87v5h8a11r71e8t8srr8le13bqu1",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
